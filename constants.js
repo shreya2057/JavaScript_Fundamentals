c43898856be0fc3f6ee03e47f165236bda31fffd
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-02 20:47:52 +0545

    Constants

---
+++ constants.js
@@ -0,0 +1,9 @@
+// const is used when value doesn't change later on the code
+// example value of pi is constant and cannot be changed
+
+let radius = 5
+const pi = 3.14 //value cannot be changed later
+let area
+
+area = pi * radius * radius
+console.log(area)
