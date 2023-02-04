a6695279e7a8970ed10210209b31fa13a6bbe0c3
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-04 20:06:18 +0545

    Arithmetic operator

---
+++ arithOperator.js
@@ -0,0 +1,16 @@
+// Arthmetic operation
+//  + -> addition of two numbers
+//  - -> substractionn of two numbers
+// add or subtract 2 boolean values -> the boolean value is converted to number i.e true to 1 and false to 0
+
+let num = 3
+num += 2 //num = num + 2
+num++   //increment operator
+
+// pre and post increment
+let x = num++ //post increment -> first assign the value of num to x and increment
+let y = ++num //pre increment -> first increment the value of num to x and then increment the value
+
+// power
+let power = Math.pow(4,3) //4-> number, 3 -> power
+let value = 4 ** 3 // ** -> power
