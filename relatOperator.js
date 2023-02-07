e5ebcdef9c62f62639ba8923042c475670611d64
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-07 22:41:54 +0545

    Relational operator

---
+++ relatOperator.js
@@ -0,0 +1,36 @@
+// Relational operator
+// Gives output in boolean format or value
+
+// > and < and >=, <=
+let data = 5 > 6
+let value = 3 < 6
+console.log(data, value) 
+
+// Comparision of two string
+// Each character has unique number(ASCII value) and those number of characters are compared. B and P are compared and so on
+let x = "Book"
+let y = "Pen"
+console.log(x < y)
+
+// Comparision of number and string
+// converts string to a number and compares
+let a = "2"
+let b = 1
+console.log(a > b)
+
+// checking if two numbers are equal ==
+let c = 2
+let d = 2
+console.log(c==d)
+
+// compares the number and string by converting string to number
+// doesnot check type
+let e = "3"
+let f = 3
+console.log(e==f)
+
+// compares data with data type -> ===
+// Use === while comparing
+let w = "4"
+let p = 4
+console.log(w===p)
