f585827ccaf875a4ccd7acd05fe2fb58e41ac64c
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-09 12:26:44 +0545

    if else condition

---
+++ if.js
@@ -0,0 +1,19 @@
+// If else condition
+// if(){
+// //statements
+// }else if (){
+// //statements
+// }else 
+// //statement
+// }
+
+let x = 6
+let y = 7
+let z = 5
+if(x > y && x > z){
+    console.log("x is greater")
+}else if(y > z){
+    console.log("y is greater")
+}else{
+    console.log("z is greater")
+}
