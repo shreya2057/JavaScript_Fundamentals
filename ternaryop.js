77a31496df56b3f560ad9e52bdf3f655ae607f90
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-09 19:30:39 +0545

    Ternary operators

---
+++ ternaryop.js
@@ -0,0 +1,15 @@
+// check whether the number is odd or even
+
+let number = 3
+let result
+if(number%2===0){
+    result = "Even"
+} else{
+    result = "Odd"
+}
+
+console.log(result)
+
+// ternary operator
+let final = number%2===0 ? "Even" : "Odd"
+console.log(final)
