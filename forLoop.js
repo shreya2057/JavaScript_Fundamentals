baf2dd284891ddc9264e246291ef33cb48cec2c8
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-10 20:59:42 +0545

    For loop

---
+++ forLoop.js
@@ -0,0 +1,13 @@
+// for loop
+
+for(let i = 0; i<=5; i++){
+    console.log("hello")
+}
+
+// Nested for loop
+for(let i=0; i<=2;i++ ){
+    console.log(`i = ${i}`)
+    for(let j = 0; j<=3; j++){
+        console.log(`j = ${j}`)
+    }
+}
