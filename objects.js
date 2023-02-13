014f6437d4e8349e91a3af8f8cbc6401619c3c9e
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-13 11:47:07 +0545

    Objects

---
+++ objects.js
@@ -0,0 +1,16 @@
+let input = "age"
+
+let student = {
+    name: "Shreya Shrestha",
+    grade: 'Bachelor',
+    age: 23,
+    'work exp': 4
+}
+
+// accessing the properties of object
+console.log(student)
+console.log(typeof student)
+console.log(student.name)
+console.log(student['grade'])
+console.log(student['work exp']) //for this square bracket must be used
+console.log(student[input])
