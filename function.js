1b51a428963e43217cdc3358cdbe9776c8497dfc
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-14 21:25:05 +0545

    Function

---
+++ function.js
@@ -0,0 +1,12 @@
+// Function -> block of reusable code
+// 2 components -> defining a function and calling a function
+
+function add(){
+    let a = 2
+    let b = 3
+    let c = a + b
+    console.log(c) 
+}
+
+
+add()
