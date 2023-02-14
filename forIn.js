e797d9143629e28d7b4b93eeb542074b950a0f6e
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-14 20:41:51 +0545

    For in loop

---
+++ forIn.js
@@ -0,0 +1,21 @@
+let programmer = {
+    name: "Shreya Shrestha",
+    tech: "JS",
+    age: 23,
+    device:{
+        device_name: "Laptop",
+        description: {
+            brand: "Lenovo",
+            ram: 8,
+            cpu: "i7"
+        }
+    }
+}
+
+for(let key in programmer){
+    console.log(key, programmer[key])
+}
+
+for(let check in programmer.device){
+    console.log(check, programmer.device[check])
+}
