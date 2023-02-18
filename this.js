661a048b9ff7d60c9dd97382d057e51a05aceb95
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-18 21:54:01 +0545

    This keyword

--- this.js
+++ this.js
@@ -9,3 +9,30 @@ let message = {
 }
 
 message.sent()
+
+let myLaptop = {
+    ram: 16,
+    cpu: "i7",
+    brand: "Hp",
+    getBrand: function (){
+        console.log(this.brand);
+    },
+    compare: function(other){
+        if(this.cpu > other.cpu){
+            this.getBrand()
+        }else{
+            other.getBrand()
+        }
+    }
+}
+
+let other = {
+    ram: 8,
+    cpu: "i9",
+    brand: "Hp",
+    getBrand: function (){
+        console.log(this.brand);
+    }
+}
+
+myLaptop.compare(other)
