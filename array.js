a239884a47bdb8799ba7299b72d1bd11cfc4fc57
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-19 19:20:10 +0545

    Array methods

--- array.js
+++ array.js
@@ -9,3 +9,17 @@ numbers[2] = 3
 console.log(numbers)
 numbers.push(true)
 console.log(numbers)
+
+//shift the elements of array to the left
+numbers.shift()
+console.log(numbers)
+
+//unshift -> adds element from left
+numbers.unshift(10)
+console.log(numbers)
+
+// Splice removes the elements from middle 
+//Splice(start index, no of elements to remove, items to be added in that place)
+
+numbers.splice(2,1,11)
+console.log(numbers)
