b4622ef0479f1fa60887cffcc89d1d45aa9908bf
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-19 16:27:42 +0545

    Array

---
+++ array.js
@@ -0,0 +1,11 @@
+// Storing data in sequential order
+let numbers = new Array()
+numbers.push(5,7,8)
+console.log(numbers)
+console.log(numbers.length)
+numbers.pop()
+console.log(numbers)
+numbers[2] = 3
+console.log(numbers)
+numbers.push(true)
+console.log(numbers)
