68846eccbb49ab0efbaa85a56514b4092cfc2626
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-20 20:14:20 +0545

    Set

---
+++ set.js
@@ -0,0 +1,20 @@
+// Set consists of unique values -> cannot access the elements using index
+// Can have values of mix datatypes
+
+let set = new Set("bookkeeper");
+console.log(set);
+
+let num = new Set();
+num.add(3);
+num.add(4);
+num.add(3);
+num.add(5);
+num.add(6);
+console.log(num);
+
+num.forEach(n=>{
+    console.log(n)
+})
+
+// .has(Value)-> search whether the value is in set or not
+console.log(num.has(3))
