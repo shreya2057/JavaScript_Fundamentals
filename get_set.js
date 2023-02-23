76ef14f1ab221fa28f13986c2e09174e05c9941a
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-02-23 13:37:30 +0545

    Getter and setter method

--- get_set.js
+++ get_set.js
@@ -15,12 +15,36 @@ class Perimeter{
 
     // Setter -> method used to set the values of the object
     set changeLength(newLength){
-        return this.length = newLength
+        return this.length = newLength;
     }
 }
 
 let rect = new Perimeter({length: 2,breadth: 3});
 console.log(rect.calcRect());
 console.log(rect.lengths);
-rect.changeLength = 5
+rect.changeLength = 5;
 console.log(rect.calcRect());
+
+// Object.defineProperty()
+
+const student= {
+    name: "Shreya"
+};
+
+// getting method
+Object.defineProperty(student, "getName", {
+    get: function(){
+        return this.name;
+    }
+});
+
+// Setting method
+Object.defineProperty(student, "setName", {
+    set: function(newName){
+        return this.name = newName;
+    }
+});
+
+console.log(student.getName);
+student.setName = "Riya";
+console.log(student.name);
