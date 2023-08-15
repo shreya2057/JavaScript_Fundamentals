4a93e551e0aab9765d1d0645095f4fbc6c64dc19
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-08-15 19:12:08 +0545

    Break and continue statement

---
+++ break_cont.js
@@ -0,0 +1,14 @@
+for(var i = 0; i<5; i++){
+    if(i===3){
+        continue;
+    }
+    console.log("Continue " + i)
+} 
+
+
+for(var i = 5; i<10; i++){
+    if(i===7){
+        break;
+    }
+    console.log("Break " + i)
+} 
