cab36bfec934b24e181a35746405cd9b74140769
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-10-20 14:46:13 +0545

    Difference between let and var

---
+++ 1_Datatypes/3_let_vs_var.js
@@ -0,0 +1,56 @@
+// Scope
+// let -> the variable declared using let can be used within the block.
+// var -> the variable declared using var can be used within the function.
+
+function letVsVar(){
+
+    let letFunction = "Variable created using let inside function"
+    var varFunction = "Variable created using var inside function"
+
+    {
+        let letBlock = "Variable created using let inside block"
+        var varBlock = "Variable created using var inside block"
+
+        console.log("Accessing outside the block")
+        console.log(letBlock) 
+        console.log(varBlock + "\n")
+    }
+
+    console.log(letFunction)   // Generates output
+    console.log(varFunction + "\n")   // Generates output
+
+    console.log("Accessing outside the block and within the function")
+    // console.log(letBlock)    // ReferenceError: letBlock is not defined
+    console.log(varBlock + "\n")   // Generates output
+}
+
+letVsVar()
+
+
+// Hoisting 
+// let -> Variables declared using let are not initialized until their
+//        definition is evaluated.
+//     -> Accessing them before the initialization results in error.
+// var -> Variables declared using var are hoisted i.e initialized with
+//        undefined.
+//     -> They can be accessed before the initialization without error.
+
+
+console.log(variable_var)
+var variable_var = "Variable declared using var"
+console.log(variable_var)
+
+// console.log(variable_let)    // ReferenceError
+var variable_let = "Variable declared using var"
+console.log(variable_let)
+
+
+// Global access
+// let -> let does not create a property on global object
+// var -> var creates a property on global object
+
+// let globalLet = "Trying to access let variable globally"
+// var globalVar = "Trying to access var variable globally"
+
+// console.log(window.globalLet)    // Cannot be accessed globally
+// console.log(window.globalVar)    // Can be accessed globally
