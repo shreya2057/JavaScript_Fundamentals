c9f8f5671f7257000a9c504111cb41d7e66a15a2
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-10-20 19:12:17 +0545

    Hello World

---
+++ 1_Hello_world/1_Hello_world.js
@@ -0,0 +1,8 @@
+// console.log -> built-in function in JavaScript used for logging 
+//                information to the console. 
+
+console.log("Hello World")
+
+
+// Used to print error
+console.error("This is the error")
