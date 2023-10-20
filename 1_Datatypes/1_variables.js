264fbe3cbc25903e86aaec3b4f1d5198a40527b5
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-10-20 13:51:12 +0545

    Variables

---
+++ 1_Datatypes/1_variables.js
@@ -0,0 +1,29 @@
+// Storing data or value temporarily, value can be changed.
+
+// let and var -> keywords that are used to create a variable.
+
+// Variable can have character, number (except in 1st position), 
+// $ and _
+
+//  JavaScript is case sensitive(i.e Name and name is different)
+
+// snake casing -> user_name
+// camel casing -> userName
+
+// Defining or creating a variable
+
+// integer or number
+let number = 5
+console.log("The value of the number is " + number)
+
+// String
+var text = "This is the text context"
+console.log(text)
+
+// Changing the value of the variable
+number = 7
+console.log("The value of the number is " + number)
+
+// Javascript is the dynamic programming language. 
+// The datatype of the variable doesnot need to be defined while creating.
+// The datatype of the variable changes according to the value assigned.
