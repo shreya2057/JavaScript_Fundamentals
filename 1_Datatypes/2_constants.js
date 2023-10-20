510b8eabc1c084901d1add836cbe3a08892286ba
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-10-20 14:11:00 +0545

    Constants

--- 1_Datatypes/2_constants.js
+++ 1_Datatypes/2_constants.js
@@ -1,5 +1,8 @@
-// const is used when value doesn't change later on the code
-// example value of pi is constant and cannot be changed
+// Constants are the variables whose value doesn't change later on the code.
+
+// const keyword is used define and create constant variable.
+
+// For example value of pi is constant and cannot be changed.
 
 let radius = 5
 const pi = 3.14 //value cannot be changed later
