5d5846b241e03985e3a096d41d08aea45c6c8370
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-10-25 16:48:48 +0545

    Operators

--- 03_Operators/01_arithmetic.js
+++ 03_Operators/01_arithmetic.js
@@ -1,15 +1,21 @@
 // Arthmetic operation
 //  + -> addition of two numbers
 //  - -> substractionn of two numbers
-// add or subtract 2 boolean values -> the boolean value is converted to number i.e true to 1 and false to 0
+// addition or subtraction of 2 boolean values 
+//      -> the boolean value is converted to number 
+//      -> i.e true to 1 and false to 0
 
 let num = 3
-num += 2 //num = num + 2
-num++   //increment operator
+num += 2 // num = num + 2
+num++   // increment operator
 
 // pre and post increment
-let x = num++ //post increment -> first assign the value of num to x and increment
-let y = ++num //pre increment -> first increment the value of num to x and then increment the value
+
+// post increment -> first assign the value of num to x and increment
+let x = num++ 
+
+// pre increment -> first increment the value of num to x and then increment the value
+let y = ++num 
 
 // power
 let power = Math.pow(4,3) //4-> number, 3 -> power
