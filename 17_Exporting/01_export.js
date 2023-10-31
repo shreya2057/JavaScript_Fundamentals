495bbc1c0df315da9eb596c47bd2dc5516defe44
Shreya Shrestha <shreyashrestha2k57@gmail.com>
2023-10-31 16:49:38 +0545

    Exporting

---
+++ 17_Exporting/01_export.js
@@ -0,0 +1,7 @@
+// export keyword is used to export
+
+export const user = {
+    name: "Name of the user",
+    email: "Email of the user",
+    username: "Username of the user"
+}
---
+++ 17_Exporting/02_import.js
@@ -0,0 +1,5 @@
+import { user } from './01_export.js'
+
+// Module is needed to support import and export
+
+console.log(user)
